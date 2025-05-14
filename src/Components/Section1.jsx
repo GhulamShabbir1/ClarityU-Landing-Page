8a16522502274916cc32f7fe14b09e55c5626a32
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-05-14 14:28:16 +0500

    1024 px responsive issue resolve

--- src/Components/Section1.jsx
+++ src/Components/Section1.jsx
@@ -5,7 +5,7 @@ import Logo3 from "../assets/L3.png";
 import Logo4 from "../assets/L4.png";
 import Logo6 from "../assets/L6.png";
 import Check from "../assets/check.png";
-import Video from "../assets/s1t.png"
+import Video from "../assets/s1t.png";
 
 import "boxicons";
 
@@ -47,17 +47,16 @@ function Section1() {
       <div className="colours">
         <div className="slide left"></div>
         <div className="center">
-          <div className="video" >
+          <div className="video">
             <img src={Video} alt="raw" />
           </div>
         </div>
         <div className="slide right"></div>
       </div>
       <div className="business">
-        <p >use daily by more then 5000 small and large businesses</p>
+        <p>use daily by more then 5000 small and large businesses</p>
       </div>
       <div className="logo-container">
-        
         <img src={Logo2} className="logo floating" alt="logo" />
         <img src={Logo3} className="logo floating" alt="logo" />
         <img src={Logo4} className="logo floating" alt="logo" />
